7e88b914f3ced17457dd78b837e31c7c41e50b51
Md Fahim Muntasir <70338542+fahimuntasir@users.noreply.github.com>
2026-01-23 03:49:02 +0600

    Create ReservationBean.java

---
+++ backend/bean/ReservationBean.java
@@ -0,0 +1,16 @@
+package net.javaguides.login.bean;
+
+public class ReservationBean {
+
+    private String flightNo;
+    private String origin;
+    private String destination;
+    private String flightClass;
+    private String fare;
+    private String passengerName;
+    private String address;
+    private String passport;
+    private String status;
+
+    // getters & setters
+}
