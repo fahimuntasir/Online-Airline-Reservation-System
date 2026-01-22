a4a04e1ccd0a7be1ca584e6bc887851220ffeace
Md Fahim Muntasir <70338542+fahimuntasir@users.noreply.github.com>
2026-01-23 03:25:08 +0600

    Update insert_data.sql

--- SQL/insert_data.sql
+++ SQL/insert_data.sql
@@ -1,21 +1,73 @@
-USE airline_databases;
-GO
+-- Insert Airline Data
+INSERT INTO Airline VALUES (3,'China Southern Airlines');
+INSERT INTO Airline VALUES (1,'American Airlines');
+INSERT INTO Airline VALUES (3,'Delta Air Lines');
+INSERT INTO Airline VALUES (22,'Frontier Airlines');
+INSERT INTO Airline VALUES (4,'Hawaiian Airlines');
+INSERT INTO Airline VALUES (10,'Air China');
+INSERT INTO Airline VALUES (3,'China Eastern Airlines');
+INSERT INTO Airline VALUES (3,'Hainan Airlines');
+INSERT INTO Airline VALUES (21,'Shenzhen Airlines');
+INSERT INTO Airline VALUES (12,'Sichuan Airlines');
+INSERT INTO Airline VALUES (1,'XiamenAir');
+INSERT INTO Airline VALUES (0,'Jetstar Asia Airways');
+INSERT INTO Airline VALUES (3,'Scoot');
+INSERT INTO Airline VALUES (13,'SilkAir');
+INSERT INTO Airline VALUES (5,'Singapore Airlines');
+INSERT INTO Airline VALUES (6,'Biman Bangladesh Airlines');
+INSERT INTO Airline VALUES (9,'US-Bangla Airlines');
+INSERT INTO Airline VALUES (11,'Regent Airways');
+INSERT INTO Airline VALUES (8,'Novoair');
+INSERT INTO Airline VALUES (2,'IndiGo');
+INSERT INTO Airline VALUES (5,'GoAir');
 
--- View Airlines
-SELECT * FROM Airline;
+-- Insert Airplane Data
+INSERT INTO Airplane VALUES ('434dd','b-757');
+INSERT INTO Airplane VALUES ('435dd','b-247');
+INSERT INTO Airplane VALUES ('464dd','b-887');
+INSERT INTO Airplane VALUES ('344dd','b-888');
+INSERT INTO Airplane VALUES ('34d5d','b-727');
+INSERT INTO Airplane VALUES ('434d4d','b-777');
+INSERT INTO Airplane VALUES ('34dd','b-744');
 
--- View Flights
-SELECT FlightCode, DepatureAirport, ArrivalAirport, Fare
-FROM Flight;
+-- Insert Airport Data
+INSERT INTO Airport VALUES (58450,'Dhaka','Bangladesh','dhsbgf');
+INSERT INTO Airport VALUES (44499,'Dhaka','Bangladesh','jakdek');
+INSERT INTO Airport VALUES (47894,'Dhaka','Bangladesh','jausgi');
+INSERT INTO Airport VALUES (78493,'Dhaka','Bangladesh','rgeghi');
+INSERT INTO Airport VALUES (52045,'Dhaka','Bangladesh','caewtt');
+INSERT INTO Airport VALUES (87820,'Dhaka','Bangladesh','xzcvsd');
 
--- Search Flights by City
-SELECT *
-FROM Flight
-WHERE DepatureAirport = 'Dhaka'
-AND ArrivalAirport = 'Wuhan';
+-- Insert Flight Data
+INSERT INTO Flight VALUES
+('FFES','China Southern','44ddw','10:34:09','07:04:09',3.2,'Beijing','Wuhan',342.3,'2021-01-28','2021-01-28');
 
--- Passenger List
-SELECT Name, PhoneNumber FROM Passenger;
+INSERT INTO Flight VALUES
+('KLHL','China Southern','44ddw','22:34:09','07:04:09',1.5,'Wuhan','Beijing',342.3,'2021-06-02','2021-06-28');
 
--- Seat Availability
-SELECT * FROM Seat WHERE Status = 1;
+INSERT INTO Flight VALUES
+('MUUI','Malindo Airline','96dsg','15:34:09','05:04:04',6.5,'Wuhan','Mumbai',862.3,'2021-02-28','2021-02-28');
+
+INSERT INTO Flight VALUES
+('DSRGFD','US Bangla','Boeng178','01:00:00','03:00:00',3.2,'Wuhan','Dhaka',3752.3,'2021-01-28','2021-01-28');
+
+-- Insert Passenger Data
+INSERT INTO Passenger VALUES ('E-32223','ZAMAN','493882NJD','5465335');
+INSERT INTO Passenger VALUES ('T-4245','MANIK','493882NJD','5658375');
+INSERT INTO Passenger VALUES ('S45W62','TOMOY','493882NJD','57643');
+INSERT INTO Passenger VALUES ('C-4567','MAMUN','493882NJD','96876553');
+INSERT INTO Passenger VALUES ('H-553G','PIAS','493882NJD','36756453');
+
+-- Insert Seat Data
+INSERT INTO Seat VALUES (1,'BUSINESS-CLASS',1);
+INSERT INTO Seat VALUES (2,'BUSINESS-CLASS',1);
+INSERT INTO Seat VALUES (10,'ECONOMY-CLASS',2);
+INSERT INTO Seat VALUES (11,'ECONOMY-CLASS',1);
+INSERT INTO Seat VALUES (12,'ECONOMY-CLASS',2);
+
+-- Insert User Data
+INSERT INTO UserAccount VALUES ('zubayer','3b56v');
+INSERT INTO UserAccount VALUES ('amitumi','64363b');
+INSERT INTO UserAccount VALUES ('misha','356bn4');
+INSERT INTO UserAccount VALUES ('sony','5b6575456');
+INSERT INTO UserAccount VALUES ('fahim','fm6fm6');
